9590d7162bf87ab1bdb6c1a56f1ffb089cb67e2a
wangyangyang <wangyangyang-015@cpic.com.cn>
2025-12-10 16:43:33 +0800

    chore: Set default locale to English, disable locale detection, and rename the project in wrangler config.

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,8 @@ import createMiddleware from 'next-intl/middleware';
 
 const intlMiddleware = createMiddleware({
   locales: ['en', 'zh'],
-  defaultLocale: 'zh'
-  // localeDetection: false
+  defaultLocale: 'en',
+  localeDetection: false
 });
 
 const isProtectedRoute = createRouteMatcher([
